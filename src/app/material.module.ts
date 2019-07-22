b55cae510ce03bcaf2cdb8c87e7d60e1c6a9df57
lucilajuliopizarro <lucitajulio@hotmail.com>
2019-07-22 17:40:03 -0400

    extrayendo cambios

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
 //aqui van los modulos que importaremos para crear lo visual con material angular
 
 
 @NgModule({
 
     imports:[
-        CommonModule
+        CommonModule,
+        BrowserAnimationsModule,
+        MatCardModule
 
     ],
     exports:[
+        BrowserAnimationsModule,
+        MatCardModule
 
     ],
-    declarations: []
+    declarations: [
+        MatCardModule
+    ]
 })
 
 
